0d92eadbabe48651db15019e832e935d788182c1
AlexisWeiss <alexissxo143@gmail.com>
2022-12-26 10:42:22 -0800

    pass onClick to button from header

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
-const Button = ({ color, text }) => {
-    return <button className='btn' style={{ backgroundColor: color }}>
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text, onClick }) => {
+    return <button className='btn' 
+                    style={{ backgroundColor: color }}
+                    onClick={onClick}>
                  {text}
             </button>;
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    color: PropTypes.string,
+    text: PropTypes.string,
+    onClick: PropTypes.func
 }
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Button from './Button';
 
 const Header = ({title}) => {
+  const onClick = () => {
+    console.log('click');
+  }
+
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button text='Add' color='green'/>
+        <Button text='Add' color='green' onClick={onClick}/>
     </header>
   )
 }
