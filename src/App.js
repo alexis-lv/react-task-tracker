a8b00c7c35a31388e759e92d2cfddc662b6123b7
AlexisWeiss <alexissxo143@gmail.com>
2022-12-28 11:50:13 -0800

    add task submit

--- src/App.js
+++ src/App.js
@@ -34,10 +34,16 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task};
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="App">
       <Header title='Task Tracker'/>
-      <AddTask />
+      <AddTask onAdd={addTask} />
       { tasks.length > 0 ? <Tasks tasks={tasks} 
                                   onDelete={deleteTask} 
                                   onToggle={toggleReminder}/> : 'No Tasks To Show'}
